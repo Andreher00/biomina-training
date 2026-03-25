3a415a17eecc66ae9a5ef03a8f3549d2f127cb14
Curro Campuzano <campuzanocurro@gmail.com>
2026-03-25 11:25:55 +0100

    Dataset trains

---
+++ datasets/belgian_train_puntuality.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import tqdm.auto as tqdm
+base_url = "https://fr.ftp.opendatasoft.com/infrabel/PunctualityHistory/Data_raw_punctuality_"
+year = 2025
+
+urls = [f"{base_url}{year}{month:02d}.csv" for month in range(1, 13)]
+
+def statistic(url):
+    df = pd.read_csv(url)
+    return df[["DELAY_ARR", "DATDEP"]].groupby("DATDEP").mean().to_numpy().flatten()
+
+stats = [statistic(url) for url in tqdm.tqdm(urls)]
+
+stats_per_day_in_minutes = np.concatenate(stats) / 60
+assert len(stats_per_day_in_minutes) == 365
+np.savetxt("belgian_train_puntuality.txt", stats_per_day_in_minutes)
